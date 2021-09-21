b5ac0628cb1577d137b872c9d5100853e99f1726
choiasher <tahun970@gmail.com>
2021-09-21 13:10:08 +0900

    first commit

---
+++ src/test/java/me/asher/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.asher.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
